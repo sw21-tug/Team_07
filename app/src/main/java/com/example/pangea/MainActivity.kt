8b1a0471e51ce368905c366d4e48323916bf9fbd
Lukas Krisper <krisper@student.tugraz.at>
2021-04-15 15:25:24 +0200

    P13 initial dashboard finished - added comment for connecting with other parts [KL,RM]

--- app/src/main/java/com/example/pangea/MainActivity.kt
+++ app/src/main/java/com/example/pangea/MainActivity.kt
@@ -13,12 +13,15 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*ATTENTION - START
+        * This starts the activity dashboard, if you don't need a button after logging in
+        * just delete the Button and use the intent and startActivity() like shown below. */
         val accounts: Button = findViewById(R.id.dashboard)
         accounts.setOnClickListener {
             val intent = Intent(this@MainActivity, DashboardsActivity::class.java)
             startActivity(intent)
         }
+        /*ATTENTION - END*/
     }
-
 }
 
