f06ddf993c837429e6260d0836506ad07b7aadc7
Daniel Rossmann <daniel.rossmann@student.tugraz.at>
2021-05-03 17:25:59 +0200

    PANGEA-P5P15P25 added changePassword tests [RD, SS]

---
+++ app/src/test/java/com/example/pangea/SettingsTests.kt
@@ -0,0 +1,30 @@
+package com.example.pangea
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class SettingsTests {
+
+    @Test
+    fun testChangePassword(){
+
+        val email = "test.user@test.com"
+        val pw = "1234abc"
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val dbHandler = DatabaseHandler()
+        dbHandler.registerUser(email, pw, context)
+        var user = dbHandler.getRegisteredUser(email, context)
+
+        Assert.assertEquals(user.password, "1234abc")
+        dbHandler.changePassword(email, "1234", context)
+        user = dbHandler.getRegisteredUser(email, context)
+        Assert.assertEquals(user.password, "1234")
+    }
+
+
+}
