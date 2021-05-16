85598e723491c4ec906155e92000f911bc7796d9
Maximilian Rindler <maximilian.rindler@student.tugraz.at>
2021-05-16 18:37:52 +0200

    PANGEA-P26 wrote Test for Plus Button [HC, RM]

--- app/src/androidTest/java/com/example/pangea/PostTests.kt
+++ app/src/androidTest/java/com/example/pangea/PostTests.kt
@@ -1,5 +1,6 @@
 package com.example.pangea
 
+import android.app.Activity
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
@@ -16,8 +17,13 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.pangea.Posts
 import com.example.pangea.R.id.sendpostbtn
+import com.facebook.AccessToken
 import com.facebook.FacebookSdk
+import com.facebook.GraphRequest
+import com.facebook.HttpMethod
+import com.facebook.login.LoginManager
 import org.hamcrest.Matchers
+import org.hamcrest.Matchers.not
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -124,6 +130,66 @@ class PostTests {
 
         onView(withId(R.id.twitterCheck)).perform(click())
         onView(withId(R.id.twitterCheck)).check(matches(isNotChecked()))
+    }
+
+    @Test
+    fun testPlusButtonNoAcc ()
+    {
+        //Intents.init()
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.example.pangea", appContext.packageName)
+
+        onView(withId(R.id.username)).perform(clearText())
+        onView(withId(R.id.username)).perform(typeText("test"))
+        onView(withId(R.id.password)).perform(clearText())
+        onView(withId(R.id.password)).perform(typeText("test"))
+
+        onView(withId(R.id.loginButton)).perform(click())
+
+
+        val emailt = "test.user@test.com"
+        val pwt = "1234abc"
+        val contextt = ApplicationProvider.getApplicationContext<Context>()
+        val dbHandlert = com.example.pangea.DatabaseHandler()
+        dbHandlert.registerUser(emailt, pwt, contextt)
+        var usert = dbHandlert.getRegisteredUser(emailt, contextt)
+
+        val thandler = TwitterHandler(contextt, usert)
+        var thasAccount = thandler.hasLinkedAccount()
+        if (thasAccount)
+        {
+            thandler.unlinkAccount()
+        }
+
+        FacebookSdk.setApplicationId("171191854853298")
+        val email = "test.user@test.com"
+        val pw = "1234abc"
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        FacebookSdk.sdkInitialize(context)
+        val dbHandler = DatabaseHandler()
+        dbHandler.registerUser(email, pw, context)
+        var user = dbHandler.getRegisteredUser(email, context)
+        junit.framework.Assert.assertEquals(email, user.email)
+        junit.framework.Assert.assertEquals(pw, user.password)
+
+
+        dbHandler.saveFacebookLink(user, null, context)
+        if(AccessToken.getCurrentAccessToken() != null)
+        {
+            GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me/permissions/", null, HttpMethod.DELETE,
+                GraphRequest.Callback {
+                    AccessToken.setCurrentAccessToken(null);
+                    LoginManager.getInstance().logOut()
+                })
+        }
+
+        onView(Matchers.allOf(ViewMatchers.withText("POSTS"), ViewMatchers.isDescendantOfA(withId(R.id.dashboard_bar))))
+            .perform(click())
+            .check(matches(isDisplayed()))
+
+        onView(withId(sendpostbtn)).check(matches(not(isClickable())));
     }
 
 }
